a98859456cafb654c7d857df9ed6df6579c7f61b
Adrian Santaella <asantaella@obscura.tail3769de.ts.net>
2026-02-07 15:40:43 -0500

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,47 @@
-import { useState, useRef } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 import Grid from './Grid'
+
 function App() {
   const wordinput = useRef(null);
+  const word = 'tooth';
 
- const handleInput = () => {
-  console.log(wordinput.current.value)
- }
+  const [currentGuess, setCurrentGuess] = useState(0);
+  const [guesses, setGuesses] = useState([
+        ["", "", "", "", ""],
+        ["", "", "", "", ""],
+        ["", "", "", "", ""],
+        ["", "", "", "", ""],
+        ["", "", "", "", ""]
+    ]
+)
+
+  const handleInput = () => {
+    let input = wordinput.current.value.split('');
+    console.log(input);
+  }
+
+  const submitGuess = () => {
+    let input = wordinput.current.value;
+    console.log(input.length);
+    if (input.length < 5) {
+      alert('You must submit a 5-letter word.');
+      return;
+    }
+
+    useEffect(() => {
+      wordinput.focus();
+    }, [])
+
+  }
 
   return (
     <>
       <h1>Puzzword</h1>
-      <Grid />
+      <Grid guesses={guesses} currentGuess={currentGuess}/>
       
-      <input type="text" ref={wordinput} maxLength={5} />
-      <button type="submit" onClick={handleInput}>Guess</button>
+      <input type="text" ref={wordinput} onInput={handleInput} maxLength={5} />
+      <button type="submit" onClick={submitGuess}>Guess</button>
       
     </>
   )
--- src/Grid.tsx
+++ src/Grid.tsx
@@ -1,19 +1,22 @@
 import Row from "./Row"
 
-function Grid() {
-    const guesses = [
-        ["a", "p", "p", "l", "e"],
-        ["", "", "", "", ""],
-        ["", "", "", "", ""],
-        ["", "", "", "", ""],
-        ["", "", "", "", ""]
-    ]
+function Grid({guesses, currentGuess}) {
+    const MAX_ROWS = 5;
 
+    const displayRows = [...guesses];
+    if (displayRows.length < MAX_ROWS) {
+        displayRows.push(currentGuess);
+    }
+
+    while (displayRows.length < MAX_ROWS) {
+        displayRows.push("")
+    }
+    
     console.log(guesses);
 
     return (
         <div className="pw-grid">
-            { guesses.map((guess, index) => <Row guess={guess} key={index} />) }
+            { displayRows.map((guess, index) => <Row guess={guess} key={index} />) }
         </div>
     )
 }
